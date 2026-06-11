09a4dbebcb4abd939f130cb5b3f6fc8484b3797f
Sumit Kumar Rai <sumitrai1811@gmail.com>
2026-06-12 01:26:44 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/phonebook";
         String user = "root";
-        String password = "sumitiitian12"; 
+        String password = "********"; 
 
         Scanner scanner = new Scanner(System.in);
 
